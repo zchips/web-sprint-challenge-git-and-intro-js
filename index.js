0723d7d78b8fcf5310f12a9d9329e9aaea0b8e8f
zchips <zchips@gmail.com>
2022-12-12 17:37:31 -0700

    final copy

--- index.js
+++ index.js
@@ -342,11 +342,17 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
+function artistInfo(array, name){
+  for(let i = 0; i <array.length; i++){
+    if(array[i].name === name){
+      return array[i].bio
+    }
+  }
   /*Your Code Here*/
 }
 
-
+artistInfo(artists, 'Frida Kahlo')
+console.log('task 8', artistInfo(artists, 'Frida Kahlo'))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use artistByCountry to do the following: 
@@ -358,11 +364,18 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, nationality){
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].nationality === nationality){
+      newArray.push(array[i].name)
+    }
+  }
+  return newArray
 }
 
-
+// // artistsByCountry(artists, 'Spanish')
+// console.log('task 10', artistsByCountry(artists, 'Spanish'))
 
 /* ***** END OF TASKS ***** */
 
